b5cf4f57b063ca409656904a10c6396def154db9
chamod wijesena <chamodwijesena77@gmail.com>
2023-03-10 11:47:23 +0530

    user image table create

---
+++ database/migrations/2023_03_10_061449_create_user_image.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserImage extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_image', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('image_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_image');
+    }
+}
